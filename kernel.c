a37084b465b797b91f0e7462002cd886d98646d4
Saksham932007 <sakshamkapoor810@gmail.com>
2025-11-17 11:58:17 +0530

    feat: Implement C kernel "print" function

--- kernel.c
+++ kernel.c
@@ -1,3 +1,12 @@
+void kprint(const char* str) {
+    char* vga = (char*)0xB8000;
+    while(*str != 0) {
+        *vga++ = *str++;
+        *vga++ = 0x07; // white on black
+    }
+}
+
 void kmain(void) {
+    kprint("Hello from 32-bit C Kernel!");
     while(1);
 }
