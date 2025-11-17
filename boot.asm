1a687af2d965d245709d9b4a756b58190b487793
Saksham932007 <sakshamkapoor810@gmail.com>
2025-11-17 11:58:50 +0530

    feat: Add 16-bit print function to bootloader

--- boot.asm
+++ boot.asm
@@ -2,6 +2,19 @@ bits 16
 org 0x7C00
 
 cli
+
+; Print subroutine
+print:
+    mov ah, 0x0e
+.loop:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .loop
+.done:
+    ret
+
 hlt
 
 times 510-($-$$) db 0
